e36975abd28baaf20b5e159ad33be809fd5ca848
Jahongir Anvarov <95640875+JaxaAnvarov@users.noreply.github.com>
2022-02-15 22:52:01 +0500

    default ranglar yigildi

---
+++ lib/core/constants/colors/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const kPrimaryBlue50Color = Color(0xFF672CBC);
+  static const kPrimaryGreyColor = Color(0xFf8789A3);
+  static const kPrimaryWhiteColor = Color(0xFFFFFFFF);
+  static const kPrimaryYellow30 = Color(0xFFF9B091);
+  static const kprimaryIndigoColor = Color(0xFF240F4F);
+
+  static const kPrimaryBlue30Color = Color(0xFFDF98FA);
+  static const kPrimaryBlue60Color = Color(0xFF9055FF);
+}
